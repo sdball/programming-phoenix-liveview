01153f70b33a4741756caf083a866bccdf54a069
Stephen Ball <sdball@gmail.com>
2021-09-10 07:37:06 -0400

    Populate seeds script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -2,10 +2,35 @@
 #
 #     mix run priv/repo/seeds.exs
 #
-# Inside the script, you can read and write to any of your
-# repositories directly:
+# The first time you run the script the rows should be inserted into the
+# database without issue. But subsequent runs will error due to the duplicate
+# SKUs unless you clean the database first.
 #
-#     Pento.Repo.insert!(%Pento.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+# The script is not using the ! versions of the repo functions so each
+# individual database execution may or may not fail.
+
+alias Pento.Catalog
+
+products = [
+  %{
+    name: "Chess",
+    description: "64 squares, 32 pieces, 2 players, 1 game",
+    sku: 5_678_910,
+    unit_price: 10.00
+  },
+  %{
+    name: "Tic-Tac-Toe",
+    description: "The classic game of Xs and Os",
+    sku: 11_112_113,
+    unit_price: 3.00
+  },
+  %{
+    name: "Table Tennis",
+    description: "Tennis but table sized",
+    sku: 21_012_34,
+    unit_price: 12.00
+  }
+]
+
+products
+|> Enum.each(&Catalog.create_product/1)
