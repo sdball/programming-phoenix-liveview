47aabadac3abae743847a85d2d69b1362623792a
Stephen Ball <sdball@gmail.com>
2021-09-26 18:47:53 -0400

    Add custom greeting to product show

--- lib/pento_web/live/product_live/show.ex
+++ lib/pento_web/live/product_live/show.ex
@@ -5,7 +5,7 @@ defmodule PentoWeb.ProductLive.Show do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(:greeting, "Hello there")}
   end
 
   @impl true
