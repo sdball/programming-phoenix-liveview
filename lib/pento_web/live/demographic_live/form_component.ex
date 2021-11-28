f94cbee1fb22fe5a4db0e2e500876bdc22199cf7
Stephen Ball <sdball@gmail.com>
2021-11-27 19:27:26 -0500

    Update demographic form to be stateful
    Now it knows when it has saved information and will send a message

--- lib/pento_web/live/demographic_live/form_component.ex
+++ lib/pento_web/live/demographic_live/form_component.ex
@@ -14,4 +14,21 @@ defmodule PentoWeb.DemographicLive.FormComponent do
   def assign_changeset(%{assigns: %{demographic: demographic}} = socket) do
     assign(socket, :changeset, Survey.change_demographic(demographic))
   end
+
+  def handle_event("save", %{"demographic" => demographic_params}, socket) do
+    IO.puts("Handling 'save' event for demographic form")
+    IO.inspect(demographic_params, label: "demographic_params")
+    {:noreply, socket |> save_demographic(demographic_params)}
+  end
+
+  defp save_demographic(socket, demographic_params) do
+    case Survey.create_demographic(demographic_params) do
+      {:ok, demographic} ->
+        send(self(), {:created_demographic, demographic})
+        socket
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        assign(socket, changeset: changeset)
+    end
+  end
 end
--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -4,10 +4,24 @@ defmodule PentoWeb.SurveyLive do
 
   @impl true
   def mount(_params, %{"user_token" => token} = _session, socket) do
-    {:ok, socket |> assign_user(token)}
+    {:ok, socket |> assign_user(token) |> assign_demographic()}
   end
 
   def assign_user(socket, token) do
     assign_new(socket, :current_user, fn -> Accounts.get_user_by_session_token(token) end)
+  end
+
+  def assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :demographic, Survey.get_demographic_by_user(current_user))
+  end
+
+  def handle_info({:created_demographic, demographic}, socket) do
+    {:noreply, socket |> handle_demographic_created(demographic)}
+  end
+
+  def handle_demographic_created(socket, demographic) do
+    socket
+    |> put_flash(:info, "Demographics updated")
+    |> assign(:demographic, demographic)
   end
 end
